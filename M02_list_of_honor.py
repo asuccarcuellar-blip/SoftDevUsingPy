5fb7b8450a0cc6faf3314f66dbc372434382445e
Andres Succar <softlaw.io@gmail.com>
2026-04-04 13:28:22 -0400

    Module 2

---
+++ M02_list_of_honor.py
@@ -0,0 +1,31 @@
+"""
+M02_list_of_honor.py
+
+This app calculates if a student is on the honor roll based on their grades. 
+
+Author: Andrés Succar
+https://github.com/asuccarcuellar-blip/SoftDevUsingPy
+
+"""
+while True:
+    # Student's name
+    name = input("Enter the student's name (or ZZZ to quit): ")
+    
+    # Exit condition
+    if name == 'ZZZ':
+        print("Exiting program...")
+        break
+    #Student's GPA
+    try:
+        gpa = float(input("Enter the student's GPA: "))
+    except ValueError:
+        print("invalid GPA. Please enter a number.\n")
+        continue
+
+    # Determine if the student is on the honor roll
+    if gpa >= 3.5:
+        print(f"{name} is on the Dean's List.\n")
+    elif gpa >= 3.25:
+        print(f"{name} is on the Honor Roll.\n")
+    else:
+        print(f"{name} did not qualify for honors.\n")
